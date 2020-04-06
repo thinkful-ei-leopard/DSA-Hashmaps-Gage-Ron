19908c56512a6ef2bf6c30820b6bbe5c1d79e0c1
Ron Martin <ronmartin89@gmail.com>
2020-04-06 13:04:11 -0700

    Completed exercise 4

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,10 +1,29 @@
 const HashMap = require('./HashMap');
 
+const WhatDoesThisDo = function(){
+  let str1 = 'Hello World.';
+  let str2 = 'Hello World.';
+  let map1 = new HashMap();
+  map1.set(str1,10);
+  map1.set(str2,20);
+  let map2 = new HashMap();
+  let str3 = str1;
+  let str4 = str2;
+  map2.set(str3,20);
+  map2.set(str4,10);
+  
+  console.log(map1.get(str1));
+  console.log(map2.get(str3));
+};
+
 function removeDupes(string) {
   let noDupes = new HashMap;
-  for(let i = 0; i < string.length; i++) {
-    console.log('stuff');
+
+  // CURRENTLY: Key: string letter, Value: order it was over written (or entered)
+  for(let i = string.length - 1; i >= 0; i--) {
+    noDupes.set(string[i], i);
   }
+  return noDupes._hashTable.sort((x, y) => x.value - y.value).map(x => x.key).join('');
 }
 
 function main() {
@@ -26,4 +45,7 @@ function main() {
   return lotr;
 }
 
-console.log(main());
+//WhatDoesThisDo();
+//console.log(removeDupes('abcb'));
+console.log(removeDupes('google'));
+//console.log(main());
